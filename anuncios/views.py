6e8a63fae6e0f713d4d2525a74112470184fdc8b
MrPowerUp82 <gustavohenrique8282@hotmail.com>
2021-04-30 16:19:08 -0300

    HTML dinamico pronto

--- anuncios/views.py
+++ anuncios/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Categoria
+from .models import Anuncio
 # Create your views here.
 def home(request):
     #
-    return render(request, 'home.html')
+    anuncios = Anuncio.objects.all()[:6]
+    categorias = Categoria.objects.all()
+    return render(request, 'home.html',{'categorias':categorias,
+    'anuncios':anuncios,
+    })
