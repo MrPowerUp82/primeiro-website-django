811668db0bf5b0d67b716048682c496fa3585761
MrPowerUp82 <gustavohenrique8282@hotmail.com>
2021-04-30 19:54:45 -0300

    Criação de links para as categorias e solucionando temporariamente as imagens

--- anuncios/views.py
+++ anuncios/views.py
@@ -8,18 +8,22 @@ from django.shortcuts import get_object_or_404
 # Create your views here.
 def home(request):
     #
+    site=""
     anuncios = Anuncio.objects.all()[:6]
     categorias = Categoria.objects.all()
     return render(request, 'home.html',{'categorias':categorias,
     'anuncios':anuncios,
+    'site':site,
     })
 
 def categoria(request,categoria_id):
     #
+    site="../"
     categorias = Categoria.objects.all()
     categoria= get_object_or_404(Categoria,id=categoria_id)
     anuncios = Anuncio.objects.filter(categoria=categoria)
     return render(request, 'home.html',{'categorias':categorias,
     'anuncios':anuncios,
     'categoria':categoria,
+    'site':site,
     })
